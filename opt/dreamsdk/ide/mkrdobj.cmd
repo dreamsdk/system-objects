55c1e45feab0021ada288dc505da52da98ac6e1a
sizious <sizious@gmail.com>
2019-02-14 11:21:08 +0100

    Implementing DirHash in mkrdobj. This generates only the romdisk if needed.

--- opt/dreamsdk/ide/mkrdobj.cmd
+++ opt/dreamsdk/ide/mkrdobj.cmd
@@ -9,6 +9,9 @@ set ScriptPath=%ScriptPath:~0,-1%
 set Runner=%ScriptPath%\..\dreamsdk-runner.exe
 if not exist %Runner% goto error_runner
 
+set DirHash=%ScriptPath%\..\helpers\dirhash.exe
+if not exist %DirHash% goto error_dirhash
+
 set ProjectPath=%1
 if "%ProjectPath%"=="" goto error_params
 if not exist %ProjectPath% goto error_params
@@ -19,22 +22,39 @@ if not exist %ObjectPath% mkdir %ObjectPath%
 
 set RomdiskImage=%RomdiskName%.img
 set RomdiskObject=%RomdiskName%.o
+set RomdiskHash=%RomdiskName%.hash
 set GenRomFsStackDumpFile=genromfs.exe.stackdump
 
 cd /D %ProjectPath%
 
-rem genromfs
+rem check if generation is necessary
+%DirHash% %RomdiskName% -nowait -quiet > %RomdiskHash%
+set /p RomdiskCurrentHash=<%RomdiskHash%
+
+set RomdiskPreviousHash=
+if exist %ObjectPath%\%RomdiskHash% set /p RomdiskPreviousHash=<%ObjectPath%\%RomdiskHash%
+
+if "%RomdiskCurrentHash%"=="%RomdiskPreviousHash%" goto no_action_needed
+
+:genromfs
 if not exist %RomdiskName% goto error_romdisk
 if exist %GenRomFsStackDumpFile% del %GenRomFsStackDumpFile%
 %Runner% /opt/toolchains/dc/kos/utils/genromfs/genromfs -f %RomdiskImage% -d %RomdiskName% -v -x .svn -x .git -x .gitkeep -x .keepme
 if exist %GenRomFsStackDumpFile% goto error_genromfs
+goto bin2o
 
-rem bin2o
+:bin2o
 %Runner% /opt/toolchains/dc/kos/utils/bin2o/bin2o %RomdiskImage% %RomdiskName% %RomdiskObject%
+goto finalize
 
-rem finalize
+:finalize
 if exist %RomdiskImage% move %RomdiskImage% %ObjectPath% > nul
 if exist %RomdiskObject% move %RomdiskObject% %ObjectPath% > nul
+if exist %RomdiskHash% move %RomdiskHash% %ObjectPath% > nul
+goto end
+
+:no_action_needed
+if exist %RomdiskHash% del %RomdiskHash%
 goto end
 
 :usage
@@ -44,6 +64,10 @@ goto end
 :error_runner
 echo DreamSDK Runner is not available...
 echo Have you moved %~n0 elsewhere?
+goto end
+
+:error_dirhash
+echo DirHash was not found...
 goto end
 
 :error_params
