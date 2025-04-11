2e4386c86aa183b6d11da7c11963e7712d11a2a9
SiZiOUS <sizious@gmail.com>
2025-04-11 10:08:54 +0200

    Forcing EN messages for MSYS2. This was easier in MSYS... See: https://stackoverflow.com/a/48781076

--- files/mingw64/etc/profile.d/000-dreamsdk.sh
+++ files/mingw64/etc/profile.d/000-dreamsdk.sh
@@ -13,7 +13,13 @@ export ENVIRONMENT_NAME=DreamSDK
 
 # Force GNU compilers messages in English language
 # This avoid some encoding problems in the console
-export LANG=en
+export LANG=en_US.UTF-8
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
 
 ################################################################################
 
